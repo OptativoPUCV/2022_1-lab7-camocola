8132fd27eccdc34f9353fe0c8cd5f319d0ec29b1
Your Name <you@example.com>
2022-05-08 23:58:10 -0400

    new try

--- heap.c
+++ heap.c
@@ -28,7 +28,7 @@ void* heap_top(Heap* pq){
 void heap_push(Heap* pq, void* data, int priority){
    if (pq->size == pq->capac)
    {
-      pq->heapArray = realloc(pq->heapArray, pq->capac*2+1);
+      pq->heapArray = realloc(pq->heapArray, sizeof(heapElem) * (pq->capac*2+1));
       pq->capac = pq->capac*2+1;
    }
    int indiceHijo = pq->size;
